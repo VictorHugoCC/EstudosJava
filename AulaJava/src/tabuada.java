83164bf8f14694b30d99e317141bd445173598ba
Victor Hugo <99856425+VictorHugoCC@users.noreply.github.com>
2022-03-22 02:06:04 -0300

    Update tabuada.java

--- AulaJava/src/tabuada.java
+++ AulaJava/src/tabuada.java
@@ -4,29 +4,29 @@ public class tabuada {
 
     public static void main(String[] args) {
 
-    //Declarando variaveis
-    int i=0, x, soma;
-    
-    //Criando objeto scan
-    Scanner scan = new Scanner(System.in);
-    
-    //Valor para as variaveis
-    System.out.println("Iae porra, digite um numero ai");
-    x=scan.nextInt();
+        // Declarando variaveis
+        int i = 0, x, soma;
 
-    //Função de repetição com for
-    for (i = 0; i < 10; i++) {
-        soma = i*x;
-        System.out.println("O valor "+x+" multiplicado por "+i+" é = "+soma);
+        // Criando objeto scan
+        Scanner scan = new Scanner(System.in);
+
+        // Valor para as variaveis
+        System.out.println("Ola, tudo bem ? Digite um numero ai");
+        x = scan.nextInt();
+
+        // Função de repetição com for
+        for (i = 0; i <= 5; i++) {
+            soma = i * x;
+            System.out.println("O valor " + x + " multiplicado por " + i + " é = " + soma);
         }
-
-        System.out.println("Testando com while:");
         
-    //Função de repetição com while
-    while (i<=20) {
-        soma = i*x;
-        i++;
-        System.out.println("O valor "+x+" multiplicado por "+i+" é = "+soma);
+        System.out.println("Testando com while: ");
+        
+        // Função de repetição com while
+        while (i <= 20) {
+            soma = i * x;
+            System.out.println("O valor " + x + " multiplicado por " + i + " é = " + soma);
+            i++;
         }
     }
 }
