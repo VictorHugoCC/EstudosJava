f0fd3496e09e870eb5e32d6778df310860eaef36
Victor Hugo <99856425+VictorHugoCC@users.noreply.github.com>
2022-03-23 03:49:22 -0300

    Update fibonacci.java

--- AulaJava/src/fibonacci.java
+++ AulaJava/src/fibonacci.java
@@ -5,20 +5,37 @@ import java.util.Scanner;
 public class fibonacci {
     public static void main(String[] args) {
 
-        int i, x = 0, soma = 1, limite;
+        int i, x = 1, y = 1, soma = 0, limite;
 
         Scanner scan = new Scanner(System.in);
 
+        // Criando limitador do laço de repitição
         System.out.print("Digite um numero: ");
         limite = scan.nextInt();
 
+        // Ajustando Limitador
+        limite = limite - 2;
+
+        // Exibir valor: 0
         System.out.println("" + soma + "");
+
+        // Exibindo primeiros valores
+        System.out.println("" + x + "");
+        System.out.println("" + y + "");
 
         for (i = 0; i < limite; i++) {
 
-            soma = x + soma;
+            // Somando valores
+            soma = x + y;
+
+            // Exibir fibonacci
             System.out.println("" + soma + "");
-            x = soma - x;
+
+            // Ajustar valor de X
+            x = y;
+
+            // Ajustar valor de Y
+            y = soma;
 
         }
     }
